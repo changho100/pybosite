10900e6c136c2f0ef6635606603926f13cff2957
changho100 <chbeak@naver.com>
2023-11-30 13:12:54 +0900

    pybo

--- pybo/views.py
+++ pybo/views.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 app_name = 'pybo'
 
 # Create your views here.
 def index(request):
-    return render(request, 'pybo/index.html')
+    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    # return render(request, 'pybo/index.html')
