3d8394e06af6388a3aac30858d4caf2cfdd40ac1
changho100 <chbeak@naver.com>
2024-01-14 00:26:02 +0900

    pybo

--- pybo/views.py
+++ pybo/views.py
@@ -5,5 +5,4 @@ app_name = 'pybo'
 
 # Create your views here.
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
-    # return render(request, 'pybo/index.html')
+    return render(request, 'pybo/index.html')
